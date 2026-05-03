2391e708f33361032c8871a7f26105f1796818ba
khs <devdev0226@gmail.com>
2026-05-04 00:58:48 +0900

    Config : vite React profiling 빌드 적용(4)

--- vite.config.ts
+++ vite.config.ts
@@ -10,17 +10,23 @@ export default defineConfig({
     port: 3001,
   },
   resolve: {
-    alias: {
-      "@": "/src",
-      "@assets": "/src/assets",
-      "@components": "/src/components",
-      "@pages": "/src/pages",
-      "@data": "/src/data",
-      "@contexts": "/src/contexts",
-      "@constants": "/src/constants",
-      "react-dom$": "react-dom/profiling",
-      "react-dom/client": "react-dom/profiling",
-      "scheduler/tracing": "scheduler/tracing-profiling",
-    },
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@data", replacement: "/src/data" },
+      { find: "@contexts", replacement: "/src/contexts" },
+      { find: "@constants", replacement: "/src/constants" },
+      { find: /^react-dom$/, replacement: "react-dom/profiling" },
+      { find: /^react-dom\/client$/, replacement: "react-dom/profiling" },
+      { find: "scheduler/tracing", replacement: "scheduler/tracing-profiling" },
+    ],
+  },
+  esbuild: {
+    keepNames: true,
+  },
+  build: {
+    minify: "esbuild",
   },
 });
